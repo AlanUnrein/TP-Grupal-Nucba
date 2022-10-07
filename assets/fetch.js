e26f68f899f7ce5c166c4313ae538fffe90f9b23
Chorchh <jorcapai@gmail.com>
2022-10-07 13:42:37 -0300

    categorias

---
+++ assets/fetch.js
@@ -0,0 +1,18 @@
+const baseURL = `https://apipizza.herokuapp.com/api/`;
+
+const requestAPI = async (categoria) => {
+  try {
+    const query = `?categoria=${categoria}`;
+    const resultado = await fetch(baseURL + query);
+
+    const json = await resultado.json();
+
+    // console.log(json);
+
+    return json;
+  } catch (error) {
+    console.error(`No se encontraron resultados` + error);
+  }
+};
+
+// requestAPI("pizza");
